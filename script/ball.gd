3b01c0ad8045f64d66b8d7638a5232b6d0b7e62f
Christer Bjoerk <Christer_Bjoerk@protonmail.com>
2026-01-12 12:38:17 +1000

    Ball
    A moving ball

---
+++ script/ball.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+const default_speed = 100
+
+var speed = default_speed
+var direction = Vector2.LEFT
+
+@onready var start_pos = position
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	speed += delta * 2
+	position += speed * delta * direction
