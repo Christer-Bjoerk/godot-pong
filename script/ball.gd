864704e1f314d4226c27b349eb11b10b13821d03
Christer Bjoerk <Christer_Bjoerk@protonmail.com>
2026-01-16 13:09:44 +1000

    Gameplay
    Ball is given a random direction after scoring a goal

--- script/ball.gd
+++ script/ball.gd
@@ -1,11 +1,13 @@
 extends Area2D
 
-const default_speed = 100
+const default_speed = 300
 
 var speed = default_speed
 var direction = Vector2.LEFT
 
 @onready var start_pos = position
+var ball_x = 0
+var ball_y = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -13,6 +15,10 @@ func _process(delta: float) -> void:
 	position += speed * delta * direction
 	
 func reset():
-	direction = Vector2.LEFT
+	
+	ball_x = randf_range(-1, 1)
+	ball_y = randf_range(-1, 1)
+
+	direction = Vector2(ball_x, ball_y)
 	position = start_pos
 	speed = default_speed
